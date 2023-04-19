2206117c115453345a7f4860542d0ad6db6d8771
okuda-zoho <130958394+okuda-zoho@users.noreply.github.com>
2023-04-19 11:29:45 +0900

    Update Book.java

--- Book.java
+++ Book.java
@@ -4,6 +4,7 @@ public class Book extends TangibleAsset {
 	public Book(String name, int price, String color, String isbn) {
 		super(name, price, color);
 		this.isbn = isbn;
+		
 	}
 	public String getIsbn() {
 		return isbn;
